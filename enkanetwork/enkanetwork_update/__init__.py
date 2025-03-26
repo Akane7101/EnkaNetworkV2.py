fcff5729ee90835a7266612266b112f1cb74408d
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2025-03-27 00:41:03 +0200

    Delete enkanetwork directory

--- enkanetwork/enkanetwork_update/__init__.py
+++
@@ -1,3 +0,0 @@
-from .config import *
-from .enka_update import *
-from .pathfinding import *
--- enkanetwork/types/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-enkanetwork.types
-~~~~~~~~~~~~~~
-Typings for the Enka Network API
-:copyright: (c) 2024-present DeviantUa
-:license: MIT, see LICENSE for more details.
-"""
